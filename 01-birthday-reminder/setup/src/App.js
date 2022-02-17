75ad2fffbdca7cf218ab02dd7091f7d259af49a9
Michelle Sauder <mgsauder@hotmail.com>
2022-02-17 15:05:38 -0800

    app

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(data)
+
+  return (<>
+    <main style={{display: 'flex', flexDirection: 'column',}}>
+      <h2>reminder project setup</h2>
+      <section className="container">
+        <div>
+          <List people={people}/>
+        </div>
+        <button onClick={()=> setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  </>);
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
       <h2>list component</h2>
+      {people.map((person, index) => { 
+        const { image, name, age, id } = person;
+        return (
+        <div className="person" key={id} >
+          <img src={image} alt={name}></img>
+          <p>{name}</p>
+          <div>{age}</div>
+        </div>)
+      })
+
+      }
     </>
   );
 };
